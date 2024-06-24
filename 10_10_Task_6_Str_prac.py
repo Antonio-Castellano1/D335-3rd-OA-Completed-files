46f1c98be0f4d3b0f07dff14daf5df54d80c3d13
Antonio Castellano <159499613+Antonio-Castellano1@users.noreply.github.com>
2024-06-24 00:25:07 -0700

    task 6

---
+++ 10_10_Task_6_Str_prac.py
@@ -0,0 +1,8 @@
+def replace_wgu(mystring):
+    new_string = mystring.replace('WGU', 'Western Governors University')
+    return print(new_string)
+
+
+replace_wgu('WGU Rocks')
+
+replace_wgu('Hello, WGU')
