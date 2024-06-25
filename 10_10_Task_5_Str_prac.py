876088098ad849d872a769b183fdf1cc8fa932be
Antonio Castellano <159499613+Antonio-Castellano1@users.noreply.github.com>
2024-06-25 00:36:44 -0700

    Delete 10_10_Task_5_Str_prac.py

--- 10_10_Task_5_Str_prac.py
+++
@@ -1,9 +0,0 @@
-# Complete the function to combine the words into a sentence and return a string
-def combine_words(words):
-    new_string = ' '.join(words)
-    return new_string
-
-
-# expected output: WGU College of IT
-print(combine_words(['WGU', 'College', 'of', 'IT']))
-print(combine_words(['Night', 'Owls', 'Rock']))
